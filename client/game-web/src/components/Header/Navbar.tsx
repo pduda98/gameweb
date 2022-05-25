9ecbe5b3ba31f5f7379d471208002570fea9f5cc
Grzegorz Cebula <grzesiekc188@gmail.com>
2022-05-25 17:39:22 +0200

    header wip

---
+++ client/game-web/src/components/Header/Navbar.tsx
@@ -0,0 +1,16 @@
+import "./Navbar.css"
+
+const Navbar=() =>{
+    return (
+        <nav className='navbar'>
+            <div className='navbar_inner'>
+                <ul>
+                    <li>GRY</li>
+                    <li>DEWELOPERZY</li>
+                    <li>GATUNKI</li>
+                </ul>
+            </div>
+        </nav>
+    )
+};
+export default Navbar;
--- client/game-web/src/index.tsx
+++ client/game-web/src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './components/Header/Header';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <Header />
+    <App />
     <App />
   </React.StrictMode>
 );
