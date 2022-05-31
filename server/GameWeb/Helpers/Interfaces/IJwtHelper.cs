6fdbc037359a8c19c10fba7f9669bf1e1a91186f
Pawel <pduda.98@gmail.com>
2022-05-31 08:29:03 +0200

    fix expirationTime

--- server/GameWeb/Helpers/Interfaces/IJwtHelper.cs
+++ server/GameWeb/Helpers/Interfaces/IJwtHelper.cs
@@ -5,6 +5,6 @@ namespace GameWeb.Helpers.Interfaces;
 public interface IJwtHelper
 {
     long? ValidateJwtToken(string? token);
-    string GenerateJwtToken(User user);
+    string GenerateJwtToken(User user, DateTime expirationTime);
     RefreshToken GenerateRefreshToken(long userId);
 }
