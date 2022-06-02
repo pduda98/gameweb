5c97763d4c4c2452e948d97da91060801e4d23a3
Grzegorz Cebula <grzesiekc188@gmail.com>
2022-06-02 03:35:06 +0200

    games filter wip

--- client/game-web/src/views/GamesView/GamesList.tsx
+++ client/game-web/src/views/GamesView/GamesList.tsx
@@ -1,25 +1,52 @@
-import { TopGamesList } from 'api/responses';
-import { useEffect, useState } from 'react';
+import { GenresList, TopGamesList } from 'api/responses';
+import { SetStateAction, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import {api} from 'api/index';
 import './GamesList.css'
 
 const GamesList: React.FC = () => {
-    const [result, setResult] = useState<TopGamesList | null>(null);
+    const [gamesResult, setResultGames] = useState<TopGamesList | null>(null);
+    const [genresResult, setResultGenres] = useState<GenresList | null>(null);
+    const [value, setValue] = useState("");
 
     useEffect(() => {
-        api.get<TopGamesList>('games').then(res => setResult(res.data))
+        api.get<TopGamesList>('games').then(res => setResultGames(res.data))
+        api.get<GenresList>('https://localhost:7205/api/v1/genres').then(res => setResultGenres(res.data))
     }, [])
 
-
-    if (result === null){
-        return <div/>;
+    function displayFilters()
+    {
+        var filter = document.getElementById("filters");
+        if(filter != null){
+            if (filter.style.display === "block") {
+                filter.style.display = "none";
+            } else {
+                filter.style.display = "block";
+            }
+        }
     }
 
-    let games = result.games;
+    const handleChange = (event: { target: { value: any; }; }) => {
+        console.log(event.target.value);
+        setValue(event.target.value);
+    }
+
+    if (gamesResult === null){
+        return <><p>No games</p><div /></>;
+    }
+
+    let games = gamesResult.games;
     return (
         <div>
-
+            <button type="button" className="collapsible" onClick={displayFilters}>Filter</button>
+            <fieldset>
+                <div id="filters">
+                    {(genresResult!==null) ? genresResult.genres.map((genre) =>
+                        <><input type="radio" name='filter' className='filterCheck' checked={value === genre} onChange={handleChange}></input><label>{genre}</label></>
+                        ) : ""
+                    }
+            </div>
+            </fieldset>
         { games.flatMap(({ id, name, averageRating, usersRating, genres}) => (
             [
                 <div className="game" key={id}>
